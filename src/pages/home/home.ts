9310ac60db3d8427cfbddeb73c48de6df8651e94
Brock Ellis <brock@sharproot.com>
2018-07-10 18:55:17 -0500

    In-class work on 7/10, working JS SDK map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NewRoutePage } from '../new-route/new-route';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,29 @@ import { NewRoutePage } from '../new-route/new-route';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public nav: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    console.log('Getting to ionViewDidLoad()');
+    this.loadMap();
+  }
+
+  loadMap(){
+    console.log("Getting to loadMap()");
+    let latLng = new google.maps.LatLng(41.422058, -96.466820);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
   }
 
