f7d0fb7176b0e9ce86ecdab7dba0f10d70f8079f
Brock Ellis <brock@sharproot.com>
2018-07-10 20:57:15 -0500

    Writing polylines and infowindows

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,31 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
+  route:any = [
+    {
+      lat: 41.421616,
+      lng: -96.465819
+    },
+    {
+      lat: 41.421624,
+      lng: -96.464529
+    },
+    {
+      lat: 41.418848,
+      lng: -96.464615
+    },
+    {
+      lat: 41.418880,
+      lng: -96.465538
+    },
+    {
+      lat: 41.421616,
+      lng: -96.465819
+    }
+  ]
+
+  infoWindow = new google.maps.InfoWindow;
+
   constructor(public nav: NavController) {
 
   }
@@ -33,6 +58,31 @@ export class HomePage {
     }
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      title: 'Hello world',
+      animation: google.maps.Animation.DROP,
+      label: 'You',
+      icon: "https://static.xx.fbcdn.net/images/emoji.php/v9/f5d/2/16/1f3c6.png"
+    });
+
+    let routeLine = new google.maps.Polyline({
+      path: this.route,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 3,
+    });
+    routeLine.setMap(this.map);
+
+    // Open the InfoWindow on mouseover:
+    google.maps.event.addListener(routeLine, 'mouseover', (event) => {
+       this.infoWindow.setPosition(event.latLng);
+       this.infoWindow.setContent('Example text');
+       this.infoWindow.open(this.map);
+    });
 
   }
 
